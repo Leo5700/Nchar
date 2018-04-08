392f5653a9f51ddceeba177d1833ce8257b8e56e
leo5700 <nomail@mail.na>
2018-04-08 03:55:40 +0300

    добавлен тест на частотность и некоторые его результаты

--- numchar 3.4.py
+++ numchar 3.4.py
@@ -6,11 +6,15 @@ import re
 
 
 key = ['р', 'бп', 'вф', 'гк', 'дт', 'жшщхцч', 'зс', 'л', 'м', 'н']
-
+key = ['нм', 'гж', 'дт', 'кх', 'чщ', 'пб', 'шл', 'сз', 'вф', 'рц']
+key = ['нл', 'рц', 'дг', 'тз', 'чкх', 'пб', 'шжщ', 'с', 'вф', 'м']
+key = ['нл', 'ф', 'д', 'тр', 'чк', 'п', 'шщ', 'с', 'в', 'бм']
+key = ['н', 'кх', 'лмр', 'т', 'чг', 'п', 'шж', 'с', 'вб', 'дз']
+key = ['л', 'н', 'вф', 'р', 'ч', 'пб', 'шжщ', 'сз', 'м', 'дт']
 
 # Читаем словарь
 
-dictName = 'dictionary.txt'  # Unicode
+dictName = 'dictionary.txt'  # Unicode only
 
 dictionary = []
 dictPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), dictName)
@@ -50,8 +54,42 @@ while True:
     print('Введите числа')
     inputstr = input()
 
-    # Тестовый режим:
+    # Тестовый режим
+
     if inputstr == 'test':
+
+        freqs = {
+        'б': 0.15,
+        'в': 0.41,
+        'г': 0.15,
+        'д': 0.27,
+        'ж': 0.09,
+        'з': 0.15,
+        'к': 0.32,
+        'л': 0.4,
+        'м': 0.29,
+        'н': 0.61,
+        'п': 0.26,
+        'р': 0.43,
+        'с': 0.5,
+        'т': 0.57,
+        'ф': 0.02,
+        'х': 0.09,
+        'ц': 0.04,
+        'ч': 0.13,
+        'ш': 0.07,
+        'щ': 0.03
+        }
+
+        keyfreqs = {}
+        for keychar in key:
+            keyfreqs.update({keychar: 0})
+            for char in tuple(keychar):
+                keyfreqs[keychar] += freqs[char]
+
+        for c in key:
+            print(c + '\t', '#' * round((keyfreqs[c]) * 10))
+
         testnumbers = range(0, 1000)
         emptycombs = []
         for i in testnumbers:
@@ -61,10 +99,11 @@ while True:
                 print(emptycombs[-1])
         print('Код не покрывает', len(emptycombs), 'из', len(testnumbers),
               'чисел от 0 до 1000')
-#        raise Exception('Закончено')
         break
 
-    # Собственно, кодирование:
+
+    # Собственно, кодирование
+
     numbers = re.findall('[0-9]+', inputstr)
     numbers = list(filter(lambda x: x != '', numbers))
 
@@ -76,4 +115,5 @@ while True:
         else:
             print(number)
             print(', '.join(selectedWords))
+
     print()
